f04881246108a4ffff01132f5eb1b60016852c96
NubarBakh <nubar.bayramli@gmail.com>
2023-11-04 06:24:45 +0400

    add more  features

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,11 +2,19 @@ let rock= document.querySelector('.Rock');
 let paper=document.querySelector('.Paper');
 let scissors= document.querySelector('.Scissors');
 
+const score ={
+    wins : 0,
+    losses:0,
+    ties :0
+}
 
 
-rock.addEventListener('click', function(){
-    let randomNumber= Math.random();
+
+
+function pickComputerMove (){
     let computerMove ='';
+    let randomNumber= Math.random();
+
     if(randomNumber >= 0 && randomNumber < 1/3) 
             {computerMove ='rock';}
         
@@ -15,64 +23,66 @@ rock.addEventListener('click', function(){
 
         else if(randomNumber >= 2/3 && randomNumber < 3/3)
             {computerMove ='scissors';};
+    
+    return computerMove;
+};
 
+function playGame(playerMove){
+    let computerMove =pickComputerMove ()
 
     let result ='';
-    if(computerMove==='rock'){result ='Tie.'}
-    else if(computerMove==='paper'){result='You lose.'}
-    else if(computerMove==='scissors'){result='You win.'}
 
-            alert (`You picked rock. Computer picked ${computerMove}. ${result} `)
+    if(playerMove==='rock'){
+        if(computerMove==='rock'){result ='Tie.'}
+        else if(computerMove==='paper'){result='You lose.'}
+        else if(computerMove==='scissors'){result='You win.'}
+
+    }
+    else if (playerMove==='paper'){
+        if(computerMove==='rock'){result ='You win.'}
+        else if(computerMove==='paper'){result='Tie.'}
+        else if(computerMove==='scissors'){result='You lose.'}
+
+    }
+    else if (playerMove==='scissors'){
+        if(computerMove==='rock'){result ='You lose.'}
+        else if(computerMove==='paper'){result='You win.'}
+        else if(computerMove==='scissors'){result='Tie.'}
+
+    }
 
 
+
+ if(result ==='You lose.'){
+        score.losses+=1}
+        else if(result==='You win.'){
+        score.wins+=1}
+        else if(result==='Tie.'){
+            score.ties+=1
+        }
+
+ 
+        /* Wins: ${score.wins}, Losses:${score.losses}, Ties: ${score.ties}*/
+
+
+    alert (`You picked ${playerMove}. Computer picked ${computerMove}. ${result} 
+    Wins: ${score.wins}, Losses:${score.losses}, Ties: ${score.ties}
+   `)
+
+    
+
+}
+
+rock.addEventListener('click', function(){
+   playGame('rock')       
 })
-
-
 
 
 paper.addEventListener('click', function(){
-    let randomNumber= Math.random();
-    let computerMove ='';
-    if(randomNumber >= 0 && randomNumber < 1/3) 
-            {computerMove ='rock';}
-        
-        else if(randomNumber >= 1/3 && randomNumber < 2/3)
-            {computerMove ='paper';}
-
-        else if(randomNumber >= 2/3 && randomNumber < 3/3)
-            {computerMove ='scissors';};
-
-
-    let result ='';
-    if(computerMove==='rock'){result ='You win.'}
-    else if(computerMove==='paper'){result='Tie.'}
-    else if(computerMove==='scissors'){result='You lose.'}
-
-            alert (`You picked paper. Computer picked ${computerMove}. ${result} `)
-
-
+    playGame('paper')  
 })
 
 scissors.addEventListener('click', function(){
-    let randomNumber= Math.random();
-    let computerMove ='';
-    if(randomNumber >= 0 && randomNumber < 1/3) 
-            {computerMove ='rock';}
-        
-        else if(randomNumber >= 1/3 && randomNumber < 2/3)
-            {computerMove ='paper';}
-
-        else if(randomNumber >= 2/3 && randomNumber < 3/3)
-            {computerMove ='scissors';};
-
-
-    let result ='';
-    if(computerMove==='rock'){result ='You lose.'}
-    else if(computerMove==='paper'){result='You win.'}
-    else if(computerMove==='scissors'){result='Tie.'}
-
-            alert (`You picked scissors. Computer picked ${computerMove}. ${result} `)
-
-
+    playGame('scissors')  
 })
 
